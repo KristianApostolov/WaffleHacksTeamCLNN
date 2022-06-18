9b037ab8cbb24e08fe0c6dc371157f0b0b1e7436
Ryan <rcheng888446@gmail.com>
2022-06-18 01:05:09 -0700

    finished tweaking the look of the navigation bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,14 +5,26 @@ import { FaCompass,FaPalette } from 'react-icons/fa'
 
 export default function Navbar() {
     return ( 
-        <div className="h-fit py-6 bg-white w-full border-b border-slate-400 flex justify-between items-center">
-            <div className=''>
-                <NavButton content="About" margin={2} />
+        <div className="h-fit py-6 flex justify-between items-center">
+            <div className='about-button'>
+                <NavButton content="&nbsp; &nbsp; &nbsp; &nbsp; artly" margin={2} />
             </div>
             <div className='inline-flex items-baseline justify-center'>
                 <NavButton content={<FaCompass className='h-6 w-6'/>} margin={2} />
+                &nbsp; &nbsp;
                 <NavButton content={<FaPalette className='h-6 w-6'/>} margin={2} />
+                &nbsp; &nbsp; &nbsp; &nbsp;
             </div>
+            <style jsx> {`
+                .h-fit {
+                    color: white;
+                    background-color: #0a6997;
+                }
+                .about-button {
+                    font-size: 1.25rem;
+                `}
+            </style>
         </div>
+
      )
 }
