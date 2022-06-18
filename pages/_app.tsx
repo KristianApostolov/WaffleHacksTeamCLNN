025004aa8a4ad21a36348120895680aecf1f94c9
Kristian Apostolov <kristianapostolovcontacts@gmail.com>
2022-06-18 04:59:14 +0300

    Changes:     - Added Firebase support     - Firebase     - Firestore     - Auth     - Hooks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {useAuthState} from 'react-firebase-hooks/auth'
 import type { AppProps } from 'next/app'
+import {auth, app, db} from '../firebase/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
+  const [user, loading, error] = useAuthState(auth)
+
   return <Component {...pageProps} />
 }
 
