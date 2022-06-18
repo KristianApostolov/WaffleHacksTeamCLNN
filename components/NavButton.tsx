eee8e8f12f26632124f21ecaf406d2bbc9465085
Larry <larrywutoronto@gmail.com>
2022-06-18 12:51:41 -0400

    navbar ui update

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -15,15 +15,14 @@ export default function NavButton({content, onClick, margin}:NavButtonProps) {
         <div 
             style={{marginLeft:margin/4+'rem', marginRight:margin/4+'rem'}} 
             onClick={()=>onClick()}
-            className="m-2 font-semibold cursor-pointer hover:text-blue-500 duration-200 text-lg">
+            className="m-2 font-semibold cursor-pointer text-lg">
             {content}
         </div>
      )
 }
 export function NavImg({src, onClick, margin}:NavImgProps) {
     return ( 
-        <img 
-            src={src} 
+        <img src={src} 
             style={{marginLeft:margin/4+'rem', marginRight:margin/4+'rem'}} 
             className="m-2 font-semibold cursor-pointer rounded-full h-8 w-8 hover:text-blue-500 duration-200 text-lg">
         </img>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import NavButton, { NavImg } from './NavButton'
 import { FaCompass,FaPalette } from 'react-icons/fa'
 import { ImExit } from 'react-icons/im'
 import { signOut } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import Router from "next/router";
 
 interface NavButtonProps {
@@ -10,7 +11,7 @@ interface NavButtonProps {
 }
 
 export default function Navbar({user}:NavButtonProps) {
-    
+
     function handleSignOut(){
         signOut(auth)
         console.log("signed out")
@@ -18,16 +19,30 @@ export default function Navbar({user}:NavButtonProps) {
     }
 
     return ( 
-        <div className="h-fit py-4 bg-white w-full border-b border-slate-300 flex justify-between items-center">
-            <div className=''>
-                <NavButton onClick={()=>undefined} content="Artly" margin={4} />
+        <div className="h-fit py-6 bg-sky-500 w-full border-b border-slate-300 flex justify-between items-center">
+            <div className='text-white font-serif'>
+                <NavButton onClick={()=>undefined} content="Artly" margin={32} />
             </div>
-            <div className='inline-flex items-baseline justify-center'>
-                <NavButton onClick={()=>undefined} content={<FaCompass className='h-8 w-8'/>} margin={4} />
-                <NavButton onClick={()=>undefined} content={<FaPalette className='h-8 w-8'/>} margin={4} />
-                {user!==null && <NavImg src={user?.photoURL} margin={4} />}
-                <NavButton content={<ImExit className='h-8 w-8'/>} onClick={handleSignOut} margin={4} />
+            <div className='divide-x-2 mr-16'>
+                <div className='inline-flex items-baseline justify-center'>
+                    <NavButton 
+                        content={<FaCompass className='h-8 w-8 fill-white opacity-70 hover:opacity-100 hover:scale-110'/>} 
+                        onClick = {() => Router.push("/explore")}
+                        margin={0} />
+                    <NavButton 
+                        content={<FaPalette className='h-8 w-8 fill-white opacity-70 hover:opacity-100 hover:scale-110'/>} 
+                        onClick = {() => Router.push(`/draw/${user?.id}`)} // Replace with id field
+                        margin={12} />
+                </div>
+                <div className='inline-flex items-baseline justify-center'>
+                    {user!==null && <NavImg src={user?.photoURL} margin={12} />}
+                    <NavButton 
+                        content={<ImExit className='h-8 w-8 fill-white opacity-70 hover:opacity-100 hover:scale-110'/>} 
+                        onClick={handleSignOut} 
+                        margin={0} />
+                </div>
             </div>
+            
         </div>
      )
 }
