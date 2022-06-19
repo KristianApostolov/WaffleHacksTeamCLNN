ee30d19c82a6f9d90e0f4b1e7190cef7741a0df2
Larry <larrywutoronto@gmail.com>
2022-06-18 20:48:15 -0400

    db stuff

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,17 +1,27 @@
 import type { NextPage, NextPageContext } from "next";
 import Canvas from "components/draw/Canvas";
+import { doc, getDoc } from "firebase/firestore";
+import { snapshotEqual } from "firebase/firestore";
+import { db } from "../../firebase/client"
 
 interface UserProps {
     id?: string;
 }
 
+async function getSnapshot() {
+    await getDoc(doc(db, "/User_info/NiymYFmYbE6oapDh1l2z"))
+        .then(snapshot => {
+            console.log(snapshot.data())
+        })
+    }
+
 const User: NextPage = ({ id }: UserProps) => {
+    getSnapshot()
     return (
         <>
             <h1>Untitled Copy of Mickey Mouse</h1>
             <div className="flex">
                 <div></div>
-                <Canvas />
             </div>
         </>
     );
