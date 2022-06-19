5b2351c10cf61361bb0f10e2cd19fb96d42aecd1
Nathan Pham <nathanpham.me@gmail.com>
2022-06-18 21:30:05 -0700

    use grid design

--- components/PaintingCard.tsx
+++ components/PaintingCard.tsx
@@ -25,11 +25,13 @@ export default function PaintingCard({
 
     return (
         <div className="inline-flex items-center justify-center flex-col m-8">
-            <h1 className="font-semibold">{heading}</h1>
+            <h1 className="font-semibold max-w-[80%] overflow-x-hidden text-ellipsis mx-auto">
+                {heading}
+            </h1>
             <Link href={`/draw/${id}`}>
                 <a>
                     <img
-                        className="h-52 w-52 rounded-3xl border border-gray-300 overflow-hidden cursor-pointer"
+                        className="h-52 w-52 mt-2 rounded-3xl border border-gray-300 overflow-hidden cursor-pointer"
                         src={painting}
                     ></img>
                 </a>
