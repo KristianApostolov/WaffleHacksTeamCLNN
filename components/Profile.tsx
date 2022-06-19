2223a152ceba57d40a9ece7c5d100357685142d3
Kristian Apostolov <kristianapostolovcontacts@gmail.com>
2022-06-19 18:52:32 +0300

    Remove Profile.tsx

--- components/Profile.tsx
+++
@@ -1,9 +0,0 @@
-async function getSnapshot() {
-    await getDoc(doc(db, "/User_info/NiymYFmYbE6oapDh1l2z"))
-        .then(snapshot => {
-
-            var username = snapshot.data()["UserStats"]["username"];
-
-            console.log(username);
-        })
-    }
